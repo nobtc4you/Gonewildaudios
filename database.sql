18b30fee400ba47ffdb6139f3f3a04ea9f3d2f95
josefina Inurrategui <josefina.inurrategui@gmail.com>
2020-09-03 19:20:48 -0300

    sql

---
+++ database.sql
@@ -0,0 +1,21 @@
+CREATE TABLE `Users` (
+    `Id` INT(19) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `User` VARCHAR(20) NOT NULL UNIQUE,
+    `Mail` VARCHAR(70) NOT NULL UNIQUE,
+    `Password` VARCHAR(20) NOT NULL,
+    `About_me` VARCHAR(200),
+    `IsAdmin` tinyint(1) NOT NULL DEFAULT 0);
+CREATE TABLE `Podcasts`(
+	`Id` INT(19) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `Title` VARCHAR(70) NOT NULL UNIQUE,
+    `Tags` VARCHAR(200),
+     `File` BLOB,
+    `Description` VARCHAR(500)
+    `UserId` int(19) NOT NULL);
+
+CREATE TABLE `Favorites`(
+	`Id` INT(19) NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    `UserId` INT(19) NOT NULL,
+    `PodcastId` INT(19) NOT NULL);
+
+
