ed85a5dc68e8224b6baa169ed1a1fd104f092681
josefina Inurrategui <josefina.inurrategui@gmail.com>
2020-09-04 12:07:32 -0300

    tag buttons in index.html ok

---
+++ javascripts/podcasts/index.js
@@ -0,0 +1,27 @@
+import {tags} from "../tags.js"
+console.log(tags)
+
+const topTags = document.getElementById("toptags")
+
+let arrayTag = []
+let i = 0
+
+while (i < 5) {
+    const n = Math.floor(Math.random() * 10)
+    const random = tags[n]
+    if(!arrayTag.includes(random) ) {
+        arrayTag.push(random);
+        i++
+        console.log(arrayTag)
+    }
+
+}
+
+arrayTag.forEach(tag => {
+    const button = document.createElement("a")
+    button.className = "tagView"
+    button.setAttribute("src", "#")
+    button.innerHTML = "# "+ tag
+    topTags.appendChild(button)
+
+})
---
+++ javascripts/tags.js
@@ -0,0 +1,3 @@
+const tags = ["economía", "ingles", "español", "informativo", "politica", "musica", "teatro", "otros1", "test", "otro", "otromas","dale", "ultimos","listo","chau"]
+
+export {tags}
