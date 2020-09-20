59e476e11dae289c8f0831e9a8bc4bdb534eb0ed
josefina Inurrategui <josefina.inurrategui@gmail.com>
2020-09-19 22:39:43 -0300

    sql corregido y completo

--- database.sql
+++ database.sql
@@ -4,13 +4,17 @@ CREATE TABLE `Users` (
     `Mail` VARCHAR(70) NOT NULL UNIQUE,
     `Password` VARCHAR(20) NOT NULL,
     `About_me` VARCHAR(200),
+    `ProfilePhoto` VARCHAR (100),
+    `Gender` VARCHAR (30),
+    `Pronouns` ENUM ('They/them','She/her','He/him','Honestly, idk yet'),
     `IsAdmin` tinyint(1) NOT NULL DEFAULT 0);
 CREATE TABLE `Podcasts`(
 	`Id` INT(19) NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `Title` VARCHAR(70) NOT NULL UNIQUE,
     `Tags` VARCHAR(200),
-     `File` BLOB,
+    `File` VARCHAR (100),
     `Description` VARCHAR(500)
+    `Script` VARCHAR(500)
     `UserId` int(19) NOT NULL);
 
 CREATE TABLE `Favorites`(
