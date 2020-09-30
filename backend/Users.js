5a6b780492f64142c2991317ccf74bcccab3c8a9
Walter Gastrell <walter.gastrell@gmail.com>
2020-09-30 20:39:59 -0300

    Changed env variables, removed unused bcrypt

--- backend/Users.js
+++ backend/Users.js
@@ -1,7 +1,11 @@
 const sequelize = require('sequelize');
-const DataBase = new sequelize(process.env.DB_URL)
+// const DataBase = new sequelize(process.env.DB_URL)
+const DataBase = new sequelize(process.env.RDS_DB_NAME, process.env.RDS_USERNAME,  process.env.RDS_PASSWORD, {
+  host: process.env.RDS_HOSTNAME,
+  dialect: 'mysql'
+});
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
+// const bcrypt = require('bcrypt');
 const saltRounds = 10;
 const AWS = require('aws-sdk');
 
@@ -21,7 +25,7 @@ module.exports ={
         }else {
             return res.status(400).json('Username already in use');
         }
-          
+
     },
     verifyMail: async (req,res) => {
         const mail = req.params.mail
@@ -34,7 +38,7 @@ module.exports ={
         }
     },
     signUp: (req, res) =>{
-             
+
     DataBase.query(
             'INSERT INTO Users (User, Mail, Password, About_me) VALUES (:User, :Mail, :Password, :About_me)',{
                 replacements: req.body
@@ -49,7 +53,7 @@ module.exports ={
         const isAdmin = await DataBase.query(`SELECT Id, User, IsAdmin FROM Users WHERE User = "${reqUser}" OR Mail = "${reqUser}"`, { type: sequelize.QueryTypes.SELECT })
         const passwordOk = password[0].Password
         const adminOk = isAdmin.find(item => item.is_admin === 1)
-       
+
         if(passwordOk == reqPass){
             if(adminOk == undefined){
                 const user = password[0]
@@ -57,7 +61,7 @@ module.exports ={
                     user
                 }, process.env.FIRM_JWT)
                 res.json({token, user})
-            }else { 
+            }else {
                 const user = isAdmin[0]
                 const token = jwt.sign({
                     user,
@@ -66,28 +70,28 @@ module.exports ={
                 res.json({token})
             }
 
-        }else { 
+        }else {
             res.json('Usuario o contraseña incorrectos!')
         }
     },
-    updateUser: (req, res) => { 
+    updateUser: (req, res) => {
         const id = req.user.user.Id
         const {password, aboutMe, photo, gender, pronouns} = req.body
         DataBase.query(`UPDATE Users SET Password = "${password}", About_me = "${aboutMe}", ProfilePhoto = "${photo}", Gender = "${gender}, Pronouns = "${pronouns}" WHERE Id = ${id}`,{type: sequelize.QueryTypes.SET})
         .then(result => (console.log(result)) || res.status(200).json("Usuario Actualizado"))
-        .catch(error => console.log(error) || res.status(400).send('Invalid data')) 
+        .catch(error => console.log(error) || res.status(400).send('Invalid data'))
     },
-    deleteUser: (req, res) => { 
-        const id = req.user.user.Id   
+    deleteUser: (req, res) => {
+        const id = req.user.user.Id
             DataBase.query(`DELETE FROM Users WHERE id = ${id}`,{type: sequelize.QueryTypes.DELETE})
             .then(result => (console.log(result)) || res.status(200).json("User eliminated."))
-            .catch(error => console.log(error) || res.status(400).send('Invalid data'))  
+            .catch(error => console.log(error) || res.status(400).send('Invalid data'))
             DataBase.query(`DELETE FROM Podcasts WHERE UserId = ${id}`,{type: sequelize.QueryTypes.DELETE})
             .then(result => (console.log(result)) || res.status(200).json("User eliminated."))
-            .catch(error => console.log(error) || res.status(400).send('Invalid data'))  
+            .catch(error => console.log(error) || res.status(400).send('Invalid data'))
             DataBase.query(`DELETE FROM Favorites WHERE UserId = ${id}`,{type: sequelize.QueryTypes.DELETE})
             .then(result => (console.log(result)) || res.status(200).json("User eliminated."))
-            .catch(error => console.log(error) || res.status(400).send('Invalid data'))  
+            .catch(error => console.log(error) || res.status(400).send('Invalid data'))
     },
     validateUserPassword: (req,res) => {
         const id = req.params.id
@@ -97,4 +101,4 @@ module.exports ={
     }
 
 
-} 
+}
